0c7bc32c27a68ac80354e9352733bcb75492158b
bilsef <bilsef1@gmail.com>
2021-06-12 21:38:30 -0700

    Update AlignFeeders.js
    Skip slots that do not have a feeder loaded.

--- Scripts/SchultzFeeders/AlignFeeders.js
+++ Scripts/SchultzFeeders/AlignFeeders.js
@@ -3,9 +3,11 @@
  * and homes in on the fiducial.  It then updates the feeder location.
  */
 
-// Load Examples/JavaScript/Utility.js functions into our scope. This will define the
+
+// Load Examples/Utility.js functions into our scope. This will define the
 // task function.
 load(scripting.getScriptsDirectory().toString() + '/Examples/JavaScript/Utility.js');
+
 
 // Import some OpenPnP classes we'll use
 var imports = new JavaImporter(org.openpnp.model, org.openpnp.util, org.openpnp.machine.reference.feeder);
@@ -14,18 +16,27 @@ var imports = new JavaImporter(org.openpnp.model, org.openpnp.util, org.openpnp.
 with (imports) {
 	task(function() {
 		var camera = machine.getDefaultHead().getDefaultCamera();
+
 		for each (var feeder in machine.getFeeders()) {
 			if (!(feeder instanceof SlotSchultzFeeder)) {
 				continue;
 			}
+			
+			name = feeder.getFeeder();
+			if (name == null) {
+				// skip if no feeder in slot
+				continue;
+			}
+
 			var location = feeder.getLocation();
 			camera.moveTo(location);
 			var fiducialPart = feeder.getFiducialPart();
 			var newLocation = feeder.getFiducialLocation(location, fiducialPart);
-      if (newLocation != null) {
-        // Store new location.  Does not change Z or rotation.
-        feeder.setLocation(new Location(LengthUnit.Millimeters, newLocation.getX(), newLocation.getY(), location.getZ(), location.getRotation()));
-      }
+			
+			if (newLocation != null) {
+				// Store new location.  Does not change Z or rotation.
+				feeder.setLocation(new Location(LengthUnit.Millimeters, newLocation.getX(), newLocation.getY(), location.getZ(), location.getRotation()));
+			}
 		}
 	});
 }
